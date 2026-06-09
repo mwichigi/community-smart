695c25b404061aa59c512e5a39fd5e67d94fd434
mwichigi <ngangamj828@gmail.com>
2026-06-09 14:54:10 +0300

    fix: set correct root and publicDir in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {}
-  },
+  root: '.',
+  publicDir: 'public',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true,
   }
 });
